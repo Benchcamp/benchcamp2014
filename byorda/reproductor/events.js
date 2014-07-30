37d1051c8fc59a5d73d61d57523334e027c27d9f
brunete <bruneti12@gmail.com>
2014-07-30 17:09:36 -0300

    +Play/Pausa... limitado

--- byorda/reproductor/events.js
+++ byorda/reproductor/events.js
@@ -1,41 +1,28 @@
-var contadorEventos, logEventos;
+var contadorEventos, controlesAuditables, logEventos, jsonCanciones, jsonAlbumes, jsonArtistas;
+
 contadorEventos = 0;
+controlesAuditables = ["btnFiltroCanciones", "btnFiltroAlbumes", "btnFiltroArtistas", "btnMute", "btnPlayPausa", "btnAtras", "btnSiguiente", "btnRepetir", "btnRandom"];
 logEventos = [];
 
+jsonCanciones = "http://localhost:8080/assets/resources/canciones.json";
+jsonAlbumes = "http://localhost:8080/assets/resources/albumes.json";
+jsonArtistas = "http://localhost:8080/assets/resources/artistas.json";
+
 document.addEventListener("DOMContentLoaded", function(e) {
-  document.getElementById("btnFiltroCanciones").addEventListener("click", registrarEvento, false);
-  document.getElementById("btnFiltroCanciones").addEventListener("click", mostrarCanciones, false);
-
-  document.getElementById("btnFiltroAlbumes").addEventListener("click", registrarEvento, false);
-  document.getElementById("btnFiltroAlbumes").addEventListener("click", mostrarAlbumes, false);
-
-  document.getElementById("btnFiltroArtistas").addEventListener("click", registrarEvento, false);
-  document.getElementById("btnFiltroArtistas").addEventListener("click", mostrarArtistas, false);
-
-  document.getElementById("btnMute").addEventListener("click", registrarEvento, false);
-
+  for (var i = 0; i < controlesAuditables.length; i++) {
+    document.getElementById(controlesAuditables[i]).addEventListener("click", registrarEvento, false);
+  };
+  
   document.getElementById("btnEventos").addEventListener("click", mostrarLogEventos, false);
+  document.getElementById("btnFiltroCanciones").addEventListener("click", mostrarCanciones, false);
+  document.getElementById("btnFiltroAlbumes").addEventListener("click", mostrarAlbumes, false);
+  document.getElementById("btnFiltroArtistas").addEventListener("click", mostrarArtistas, false);
 });
-
-// GET falla a menos que la aplicación esté en el mismo dominio que los assets.
-function getJson(path, callback) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4) {
-            if (httpRequest.status === 200) {
-                var data = JSON.parse(httpRequest.responseText);
-                if (callback) callback(data);
-            }
-        }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send();
-}
 
 function registrarEvento(e) {
   var evento = {
     accion: e.type,
-    elemento: e.target,
+    elemento: e.target.name,
     hora: new Date().getTime()
   };
   logEventos.push(evento);
@@ -47,7 +34,6 @@ function incrementarCantEventos() {
   document.getElementById("lblContadorEventos").innerHTML = contadorEventos;
 }
 
-//Podrían combinarse en una función única parametrizada, separando el código que llena la tabla en funciones aparte.
 function mostrarLogEventos() {
   var tablaContenido = document.getElementById("tabla-contenido");
 
@@ -55,100 +41,19 @@ function mostrarLogEventos() {
   tablaContenido.innerHTML += "<tr id=\"cabezal-contenido\"><th>Acci&oacute;n</th><th>Elemento</th><th>Hora</th></tr>";
 
   for (var i = 0; i < logEventos.length; i++) {
-    tablaContenido.innerHTML += "<tr><td>" + logEventos[i].accion + "</td><td>" + logEventos[i].elemento.id + "</td><td>" + formatearHora(logEventos[i].hora) + "</td></tr>";
+    tablaContenido.innerHTML += "<tr><td>" + logEventos[i].accion + "</td><td>" + logEventos[i].elemento + "</td><td>" + formatearHora(logEventos[i].hora) + "</td></tr>";
   }
 }
 
 function mostrarCanciones() {
-  var tablaContenido = document.getElementById("tabla-contenido");
-
-  tablaContenido.innerHTML = "";
-  tablaContenido.innerHTML += "<tr><th>Pista</th><th>Artista</th><th>Tiempo</th><th>&Aacute;lbum</th></tr>";
-
-  //Llenar tabla con las canciones
-  getJson("http://localhost:8080/assets/resources/canciones.json", function(data){
-      for (var i = 0; i < data.canciones.length; i++) {
-        var cancion, tablaContenido, row, celdaNombre, celdaArtista, celdaTiempo, celdaAlbum;
-
-        cancion = data.canciones[i];
-
-        tablaContenido = document.getElementById("tabla-contenido");
-        
-        row = document.createElement("tr");
-
-        celdaNombre = document.createElement("td");
-        celdaArtista = document.createElement("td");
-        celdaTiempo = document.createElement("td");
-        celdaAlbum = document.createElement("td");
-        
-        celdaNombre.innerHTML = cancion.nombre;
-        celdaArtista.innerHTML = cancion.artista;
-        celdaTiempo.innerHTML = cancion.tiempo;
-        celdaAlbum.innerHTML = cancion.album;
-
-        row.appendChild(celdaNombre);
-        row.appendChild(celdaArtista);
-        row.appendChild(celdaTiempo);
-        row.appendChild(celdaAlbum);
-
-        tablaContenido.appendChild(row);
-      };
-  });
+  mostrarContenidoJSON(jsonCanciones);
 }
 
 function mostrarAlbumes() {
-  var tablaContenido = document.getElementById("tabla-contenido");
-
-  tablaContenido.innerHTML = "";
-  tablaContenido.innerHTML += "<tr><th>Artista</th><th>&Aacute;lbum</th></tr>";
-
-  //Llenar tabla con los albumes
-  getJson("http://localhost:8080/assets/resources/albumes.json", function(data){
-      for (var i = 0; i < data.albumes.length; i++) {
-        var album, tablaContenido, row, celdaNombre, celdaArtista;
-
-        album = data.albumes[i];
-
-        tablaContenido = document.getElementById("tabla-contenido");
-        row = document.createElement("tr");
-        celdaArtista = document.createElement("td");
-        celdaNombre = document.createElement("td");
-        
-        celdaArtista.innerHTML = album.artista;
-        celdaNombre.innerHTML = album.nombre;
-
-        row.appendChild(celdaArtista);
-        row.appendChild(celdaNombre);
-
-        tablaContenido.appendChild(row);
-      };
-  });
+  mostrarContenidoJSON(jsonAlbumes);
 }
 
 function mostrarArtistas() {
-  var hrCabezal, tablaContenido;1
-
-  tablaContenido = document.getElementById("tabla-contenido");
-
-  tablaContenido.innerHTML = "";
-  tablaContenido.innerHTML += "<tr><th>Artista</th></tr>";
-
-  //Llenar con los artistas
-  getJson("http://localhost:8080/assets/resources/artistas.json", function(data){
-      for (var i = 0; i < data.artistas.length; i++) {
-        var artista, tablaContenido, row, celdaNombre;
-
-        artista = data.artistas[i];
-
-        tablaContenido = document.getElementById("tabla-contenido");
-        row = document.createElement("tr");
-        celdaNombre = document.createElement("td");
-        
-        celdaNombre.innerHTML = artista.nombre;
-
-        row.appendChild(celdaNombre);
-
-        tablaContenido.appendChild(row);
-      };
-  });
+  mostrarContenidoJSON(jsonArtistas);
 }
+
--- byorda/reproductor/util.js
+++ byorda/reproductor/util.js
@@ -1,3 +1,17 @@
+function mostrarContenidoJSON(path) {
+    var httpRequest = new XMLHttpRequest();
+    var retorno;
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) { //DONE
+            if (httpRequest.status === 200) { //SUCCESS
+                mostrarContenido(JSON.parse(httpRequest.responseText));
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+
 function formatearHora(timestamp) {
 	var inDate = new Date(timestamp);
 	return dosDigitos(inDate.getHours()) + ":" + dosDigitos(inDate.getMinutes()) + ":" + dosDigitos(inDate.getSeconds());
@@ -5,4 +19,55 @@ function formatearHora(timestamp) {
 
 function dosDigitos(numero) {
 	return numero > 9 ? numero : "0" + numero;
+}
+
+function mostrarContenido(jsonDatos) {
+  var divContenido, tablaContenido;
+  
+  tablaContenido = crearTabla(jsonDatos);
+  divContenido = document.getElementById("contenido");
+
+  tablaContenido.id = "tabla-contenido";
+  divContenido.innerHTML = "";
+  divContenido.appendChild(tablaContenido);
+}
+
+function crearTabla(jsonDatos) {
+    var tabla, tr;
+    
+    tabla = document.createElement("table");
+    tr = document.createElement("tr");
+
+    for (var name in jsonDatos.items[0]) {
+        var th = document.createElement("th");
+        th.innerHTML = name;
+        tr.appendChild(th);
+    }
+
+    tabla.appendChild(tr);
+
+    for (var i = 0; i < jsonDatos.items.length; ++i) {
+        tr = document.createElement("tr");
+        tr.addEventListener("click", seleccionarFila, false)
+
+        for (var name in jsonDatos.items[i]) {
+            var td = document.createElement("td");
+            td.innerHTML = jsonDatos.items[i][name];
+            tr.appendChild(td);
+        }
+        tabla.appendChild(tr);
+    }
+    return tabla;
+}
+
+function seleccionarFila() {
+  deseleccionarTodo();
+  this.className = "selected";
+}
+
+function deseleccionarTodo() {
+    var rows = document.querySelectorAll("#tabla-contenido tr");
+    for (var i = 1; i < rows.length; i++) {
+        rows[i].className = "";
+    }
 }
