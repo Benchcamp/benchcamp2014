e087576416031d2965015e6e861092b85e4e254a
carlosaiello <carlos.aiello@globant.com>
2014-08-14 16:43:31 -0300

    pires+++

--- caiello/js/songapi.js
+++ caiello/js/songapi.js
@@ -11,7 +11,6 @@ Track.prototype.setLyricCache = function (lyric){
 	this.lyric_cache=lyric;
 }
 
-
 /*
 Album
 */
@@ -19,8 +18,8 @@ function Album(title, year, cover){
 	this.title	  = title || "";
 	this.year     = year  || "";
   this.cover    = cover || "nocover.png";
+  this.tracks    = [];
 }
-Album.prototype.tracks  = [];
 Album.prototype.pushTrack = function (track){
     this.albums.push(album);
 }
@@ -28,14 +27,14 @@ Album.prototype.setTracks = function (tracks){
     this.tracks=tracks;
 }
 
-
 /*
 Artist
 */
 function Artist(name){
 	this.name  = name;
+  this.albums=[];
 }
-Artist.prototype.albums = [];
+//Artist.prototype.albums = [];
 Artist.prototype.pushAlbum = function (album){
     this.albums.push(album);
 }
@@ -46,82 +45,103 @@ Artist.prototype.setAlbums = function (albums){
 
 
 
-var tracklist=[];
-tracklist.push(new Track(1, 4, "Enter Sandman", "Metallica"));
-tracklist.push(new Track(2, 3, "The unforgiven", "Metallica"));
-
-var album = new Album(1, 2, 1984, "cover.png");
-
-for (var i=0; i<tracklist.length; i++)
-	console.log(tracklist[i].title);
 
 
 
-//aca ver de hacer Promise.all, tenndria q pasar un array de urls, 
-// y hacer un foreach de los new promises apendeandolas?
-function get(url) {
-  return new Promise(function(resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status == 200)
-        resolve(JSON.parse(req.response));
-      else
-        reject(Error(req.statusText));
-    };
-    req.onerror = function() {
-      reject(Error("Network Error"));
-    };
-    req.send();
-  });
-}
-
-
-// var p1=get(path).then(function(response) {
-//     printit(response);
-// }, function(error) {
-//   console.error("Failed!", error);
-// });
-
-function printit(data){
-    console.log(data["albums"][0]["artist"]);
-
-}
-
-
-function logueate(esta)
-{
-  console.log(esta);
-}
 
 
 
-var path="assets/json/artistsv2.json";
-var path2="assets/json/albumsv2.json";
-var path3="assets/json/songsv2.json";
-var promises = [get(path),get(path2),get(path3)];
 
 
-// transforms a parsed json to an array of artists (artists have albums and albums have songs)
-function jsonsToObjects(jsons){
-  console.log("creating objects...");
-  var artists=[];
-  var artistsjs=jsons[0]["artists"];
-  for (var i=0; i<artistsjs.length; i++){
-    artists.push(new Artist(artistsjs[i]["artist"]));
+
+
+
+
+
+
+
+
+var chongo="hola";
+
+
+
+//Events Logger Module
+var dataLoad = (function () {
+  
+  var _artists;
+
+  // gets a url creating a promise
+  function _get(url) {
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status == 200)
+          resolve(JSON.parse(req.response));
+        else
+          reject(Error(req.statusText));
+      };
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+      req.send();
+    });
   }
-  console.log("...objects created!");
-}
+
+  function _getArtists(){
+    return _artists;
+  }
+
+  function _createObjects(art_path, alb_path, sng_path){
+    var _promises = [_get(art_path), _get(alb_path), _get(sng_path)];
+
+    Promise.all(_promises).then(function(resultados) {
+
+      return _jsonsToObjects(resultados);
+    }, function() {
+      console.log("error");
+    }).then(function(r){ console.log(_artists===r)  });
+  }
+  // transforms a parsed json to an array of artists (artists have albums and albums have songs)
+  function _jsonsToObjects(jsons){
+    _artists = [];
+    var artistsjs=jsons[0]["artists"];
+    var albumsjs=jsons[1]["albums"];
+    var songsjs=jsons[2]["tracks"];
+    // using hashtables for performance (3*n)..
+    // loading artists
+    for (var i=0; i<artistsjs.length; i++)
+      //uso una hashtable para mejorar performance
+      _artists[artistsjs[i]["artist"]]= new Artist(artistsjs[i]["artist"]);
+
+    // loading albums
+    for (var i=0; i<albumsjs.length; i++)
+      _artists[ albumsjs[i]["artist"] ].albums[ albumsjs[i]["album"] ] = new Album(albumsjs[i]["album"], albumsjs[i]["year"], albumsjs[i]["cover"]);
+    
+    // loading tracks
+    for (var i=0; i<songsjs.length; i++)
+      _artists[ songsjs[i]["artist"] ].albums[ songsjs[i]["album"] ].tracks[ songsjs[i]["song"] ] = new Track(songsjs[i]["song"], songsjs[i]["song"] );
+
+    return _artists;
+  }
+
+    // Reveal
+    return {
+        createObjects: _createObjects,
+        getArtists: _getArtists,
+        getPromiseJSON: _get
+    };
+})();
 
 
 
-Promise.all(promises).then(function(resultados) {
-  jsonsToObjects(resultados);
- // console.log(resultados);
- // console.log(resultados[0]["albums"][0]["artist"]+" "+resultados[1]["artists"][0]["artist"]);
-}, function() {
-  console.log("error");
-}).then(function(){logueate("esta")});
+
+
+
+
+
+dataLoad.createObjects("assets/json/artistsv2.json","assets/json/albumsv2.json","assets/json/songsv2.json");
+
 
 // printit("algo",chongo);
 // function printit(algo, callback){
