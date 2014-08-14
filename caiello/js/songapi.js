959fe7c6b1d35926ca572ad2a458473f3efac6d0
carlosaiello <carlos.aiello@globant.com>
2014-08-14 10:55:25 -0300

    pires++

--- caiello/js/songapi.js
+++ caiello/js/songapi.js
@@ -18,7 +18,7 @@ Album
 function Album(title, year, cover){
 	this.title	  = title || "";
 	this.year     = year  || "";
-    this.cover    = cover || "nocover.png";
+  this.cover    = cover || "nocover.png";
 }
 Album.prototype.tracks  = [];
 Album.prototype.pushTrack = function (track){
@@ -77,20 +77,51 @@ function get(url) {
 }
 
 
-var path="assets/json/albumsv2.json";
-
-get(path).then(function(response) {
-    printit(response);
-}, function(error) {
-  console.error("Failed!", error);
-});
-
+// var p1=get(path).then(function(response) {
+//     printit(response);
+// }, function(error) {
+//   console.error("Failed!", error);
+// });
 
 function printit(data){
     console.log(data["albums"][0]["artist"]);
 
 }
 
+
+function logueate(esta)
+{
+  console.log(esta);
+}
+
+
+
+var path="assets/json/artistsv2.json";
+var path2="assets/json/albumsv2.json";
+var path3="assets/json/songsv2.json";
+var promises = [get(path),get(path2),get(path3)];
+
+
+// transforms a parsed json to an array of artists (artists have albums and albums have songs)
+function jsonsToObjects(jsons){
+  console.log("creating objects...");
+  var artists=[];
+  var artistsjs=jsons[0]["artists"];
+  for (var i=0; i<artistsjs.length; i++){
+    artists.push(new Artist(artistsjs[i]["artist"]));
+  }
+  console.log("...objects created!");
+}
+
+
+
+Promise.all(promises).then(function(resultados) {
+  jsonsToObjects(resultados);
+ // console.log(resultados);
+ // console.log(resultados[0]["albums"][0]["artist"]+" "+resultados[1]["artists"][0]["artist"]);
+}, function() {
+  console.log("error");
+}).then(function(){logueate("esta")});
 
 // printit("algo",chongo);
 // function printit(algo, callback){
