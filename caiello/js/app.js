4df4528b705018ac628d914d2e6ac1d4a28b4c81
carlosaiello <carlos.aiello@globant.com>
2014-08-19 14:20:06 -0300

    albums, artists and tracks filters + data load from json

--- caiello/js/app.js
+++ caiello/js/app.js
@@ -1,12 +1,15 @@
 "use strict";
 
-var app=angular.module("player",["player.controllers"]);
+var app=angular.module("player",["player.controllers","ngRoute"]);
+var controllers = angular.module("player.controllers", []);/*otros modulos dependientes o requeridos*/
 
 
-// app.directive('helloWorld', function() {
-//   return {
-//       restrict: 'AEC',
-//       replace: 'true',
-//       template: '<h3>Hello World!!</h3>'
-//   };
-// });
+app.config(function ($routeProvider){
+	$routeProvider
+	.when("/",{controller:"AlbumsListCtrl", templateUrl:"partials/albums.html"})
+	.when("/albums",{controller:"AlbumsListCtrl", templateUrl:"partials/albums.html"})
+	.when("/artists",{controller:"ArtistsListCtrl", templateUrl:"partials/artists.html"})
+	.when("/tracks",{controller:"TracksListCtrl", templateUrl:"partials/tracks.html"})
+	.otherwise({redirectTo:"/"})
+});
+
--- caiello/js/controllers.js
+++ caiello/js/controllers.js
@@ -1,32 +1,37 @@
-'use strict';
+"use strict";
 
-var controllers = angular.module("player.controllers", []);/*otros modulos dependientes o requeridos*/
-
-controllers.controller("AppCtrl", function($scope){
-	$scope.nombre="Nothing Filtered";
-	$scope.filterTracks= function(){
-		$scope.nombre="Filter Tracks";
-	}
-	$scope.filterAlbums= function(){
-		$scope.nombre="Filter Albums";
-	}
-	$scope.filterArtists= function(){
-		$scope.nombre="Filter Artists";
-	}
-} 
-);
-
-//Artist List Service?
+//Artist List Controller
 controllers.controller('ArtistsListCtrl', function($scope, $http){
-	$scope.list;
+	$scope.artists;//creo que no es necesario...
 
 	//returns a promise
 	$http.get("assets/json/artistsv2.json").
-	success( function (data){ 
-		$scope.list=data.data;
-		console.log(data);
-	}).
-	error(function(data) {
-		console.log("error loading json artists");
+	then( function (res){
+		$scope.artists=res.data.artists;
 	});
 })
+
+//Artist List Service?
+controllers.controller('AlbumsListCtrl', function($scope, $http){
+	$scope.albums;
+
+	//returns a promise
+	$http.get("assets/json/albumsv2.json").
+	then( function (res){
+		$scope.albums=res.data.albums;
+	});
+})
+
+
+//Tracks
+controllers.controller('TracksListCtrl', function($scope, $http){
+	$scope.tracks;
+
+	//returns a promise
+	$http.get("assets/json/songsv2.json").
+	then( function (res){
+		$scope.tracks=res.data.tracks;
+	});
+})
+
+
