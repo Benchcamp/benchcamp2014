e94e0b378a96339a9f50391a9143388d5ed28ecf
carlosaiello <carlos.aiello@globant.com>
2014-08-29 17:27:45 -0300

    fix

--- caiello/js/directives/carate.js
+++ caiello/js/directives/carate.js
@@ -5,6 +5,18 @@ directives.directive("caRate", function() {
     return {
         restrict: "E",
         transclude: true,
-        templateUrl: 'views/directives/carate.html'
+        templateUrl: 'views/directives/carate.html',
+        link:function(scope, elements, attrs){
+        	scope.stars =[];
+
+        	for (var i=0; i<5; i++){
+                scope.stars.push(false);
+        	}
+        	
+        	scope.selectRating = function(star){
+        		//stub
+            }
+
+        }
     }
 })
